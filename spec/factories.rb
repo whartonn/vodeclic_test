7b4c5da3cb2edb2fc4a923bb5e5e027fa582ecc0
N Wharton <wharton.n@gmail.com>
2015-08-19 15:29:22 +0200

    setting up company testing

--- spec/factories.rb
+++ spec/factories.rb
@@ -11,4 +11,17 @@ FactoryGirl.define do
     name "SpeedRun"
     fa_symbol  "bolt"
   end
+  
+  factory :company_1, class: Company do
+    name "Vodeclic"
+  end
+  factory :company_2, class: Company do
+    name 'thoughtbot'
+  end
+  factory :company_3, class: Company do
+    name 'Basecamp'
+  end
+  
+  
 end
+
