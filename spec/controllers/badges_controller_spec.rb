7a2947e9cb44a61446c1c5f6aaa745d461c911b2
N Wharton <wharton.n@gmail.com>
2015-08-19 15:13:16 +0200

    added / converted all pre-made badge tests

--- spec/controllers/badges_controller_spec.rb
+++ spec/controllers/badges_controller_spec.rb
@@ -20,4 +20,45 @@ RSpec.describe BadgesController, :type => :controller do
       expect(response).to have_http_status(:success)
     end
   end
+  
+  describe "POST badge" do
+    it "should create badge" do
+      expect{
+        post :create, badge: { fa_symbol: @badge.fa_symbol, 
+          name: @badge.name }
+      }.to change{Badge.count}.by(1)
+      expect(response).to redirect_to(badge_path(assigns(:badge)))
+    end
+  end
+  
+  describe "GET badge" do
+    it "should show the appropriate badge" do
+      get :show, id: @badge.id
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template :show
+    end
+  end
+
+  describe "edit badge" do
+    it "should edit a badge" do
+      get :edit, id: @badge
+      expect(response).to have_http_status(:success)
+    end
+  end
+  
+  describe "PATCH badge" do
+    it "should update badge" do
+      patch :update, id: @badge, badge: {fa_symbol: @badge.fa_symbol, name: @badge.name }
+      expect(response).to redirect_to(badge_path(assigns(:badge)))
+    end
+  end
+
+  describe "DELETE badge" do
+    it "should delete badge" do
+      expect{
+        delete :destroy, id: @badge
+      }.to change{Badge.count}.by(-1)
+    end
+  end
+  
 end
