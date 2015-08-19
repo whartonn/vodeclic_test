897381672e7ecf6652022b8571b164cf2ea8b49f
N Wharton <wharton.n@gmail.com>
2015-08-19 12:25:14 +0200

    adding count of users with a badge per company

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,7 @@ class Company < ActiveRecord::Base
   has_many :users
 
   def badges
-    Badge.joins(:badge_users => {:user => :company}).where(:users => {:company_id => self.id}).select("badges.*")
+    Badge.joins(:badge_users => {:user => :company}).where(:users => {:company_id => self.id}).select("badges.*, badge_users.id as badge_users_id, COUNT(badge_users.id) as badge_cnt").group("badges.name")
   end
 
   def to_s
