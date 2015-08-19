2b95aa56902cc1bd5f2e72f21e579dfcdad4e302
N Wharton <wharton.n@gmail.com>
2015-08-19 12:23:34 +0200

    creating query for badge users in a company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,10 @@
 class Company < ActiveRecord::Base
   has_many :users
 
+  def badges
+    Badge.joins(:badge_users => {:user => :company}).where(:users => {:company_id => self.id}).select("badges.*")
+  end
+
   def to_s
     name
   end
