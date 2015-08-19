8478da277f10ba57820b4996636862086c7da9ce
N Wharton <wharton.n@gmail.com>
2015-08-19 15:53:02 +0200

    added factories of various types

--- spec/factories.rb
+++ spec/factories.rb
@@ -21,7 +21,43 @@ FactoryGirl.define do
   factory :company_3, class: Company do
     name 'Basecamp'
   end
-  
-  
+
+  factory :video_1, class: Video do
+    name 'Ruby variables'
+  end
+  factory :video_2, class: Video do
+    name 'Ruby operators'
+  end
+  factory :video_3, class: Video do
+    name 'Ruby methods'
+  end
+  factory :video_4, class: Video do
+    name 'Ruby classes'
+  end
+    
+  factory :user_1, class: User do
+    name 'Thomas' 
+    company :company_1
+    videos [:video_1,:video_2,:video_3,:video_4]
+    badges [:badge_1,:badge2]
+  end
+  factory :user_2, class: User do
+    name 'Guillaume' 
+    company :company_1
+    videos [:video_1,:video_2]
+    badges [:badge_1]
+  end
+  factory :user_3, class: User do
+    name 'David Heinemeier Hansson' 
+    company :company_2
+    videos [:video_4]
+    badges [:badge_3]
+  end
+  factory :user_4, class: User do
+    name 'Joe Ferris' 
+    company :company_2
+    videos [:video_4]
+    badges [:badge_3]
+  end
 end
 
